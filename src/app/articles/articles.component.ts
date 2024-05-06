013a8b66873a231cc4b68d90b7339f1b41626646
David Saga <epostsaga@gmail.com>
2024-05-06 14:28:28 +0200

    aritcles connected to scully-posts

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.scss'
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit{
+  constructor(private scullyRouSer: ScullyRoutesService) { }
+  posts$: Observable<ScullyRoute[]> | undefined;
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  this.posts$=this.scullyRouSer.available$.pipe(map(posts=>posts.filter(post=>post.title)))
+}
 
 }
+
+
