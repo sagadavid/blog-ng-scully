c6705d75ecb2cf898c936cfc6cafc409e50a580f
David Saga <epostsaga@gmail.com>
2024-05-06 12:23:02 +0200

    contact module-component, routerLinked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, CoreModule, SharedModule
+    AppRoutingModule, CoreModule, SharedModule, ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
